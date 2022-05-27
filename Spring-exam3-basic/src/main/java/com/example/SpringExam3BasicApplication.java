50071e8a15de13a3e2dc6eec28cc8842fcba5429
naka_rikito <22naka_rikito@axiz.co.jp>
2022-05-27 16:17:40 +0900

    origin

---
+++ Spring-exam3-basic/src/main/java/com/example/SpringExam3BasicApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExam3BasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExam3BasicApplication.class, args);
+	}
+
+}
---
+++ Spring-exam3-basic/src/test/java/com/example/SpringExam3BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExam3BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
